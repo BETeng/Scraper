f7075209dc2f99d7d7fcc5244a13540099e24274
BETeng <brian7.teng@gmail.com>
2019-07-20 01:23:21 -0500

    some results

--- server.js
+++ server.js
@@ -18,11 +18,56 @@ db.on("error", function(error) {
   console.log("Database Error", error);
 });
 
-app.get("/", function(req,res){
+app.get("/", function(req, res){
   res.send("Hello world")
 })
 
-
-app.listen(6000, function() {
-  console.log("App listening on: http://localhost:6000");
+app.get("/all", function(req, res) {
+  db.scrapedData.find({}, function(error, found) {
+    if (error) {
+      console.log(error);
+    }
+    else{
+      res.json(found);
+    }
+  });
 });
+
+
+app.listen(4000, function() {
+  console.log("App listening on: http://localhost:4000");
+});
+
+// scraper needs to scrape: headline / summary / url 
+  // need to use axios to call the site and return a response
+  // parse response.data using cheerio
+  //  document.querySelector("#site-content > div:nth-child(3) > div:nth-child(5)")
+app.get("/scrape", function(req, res) {
+  axios.get("https://www.theonion.com/").then(function(response){
+    const $ = cheerio.load(response.data);
+  
+    // title of article
+    // $(".sc-759qgu-0").each(function(i, element){
+    //   var title = $(element).text();
+    //   console.log(title)
+    // })
+    // $(".js_link sc-1out364-0 fwjlmD").each(function(i, element){
+    //   var link = $(element).attr("href")
+    //   console.log(link)
+    // })
+    
+    $("article").each(function(i, element){
+      var link = $(element).children("a").attr("href");
+      console.log(link)
+      var title = $(element).children("a").text()
+      console.log(title)
+    })
+
+    // $(".sc-3kpz01-7").each(function(i, element){
+    //   // var title = $(element).children("a").text();
+    //   var link = $(element).children("a").attr("href");
+    //   console.log(link)
+    //   console.log(title)
+    // })
+  })
+})
