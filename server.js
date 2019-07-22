fc6d7f8d8a5ea16c42cd7dea024a0fdf076bab15
BETeng <brian7.teng@gmail.com>
2019-07-22 15:49:32 -0500

    articles save to db

--- server.js
+++ server.js
@@ -14,64 +14,73 @@ const databaseUrl = "scraper";
 const collections = ["scrapedData"];
 
 const db = mongojs(databaseUrl, collections);
-db.on("error", function(error) {
+db.on("error", function (error) {
   console.log("Database Error", error);
 });
 
-app.get("/", function(req, res){
+app.get("/", function (req, res) {
   res.send("Hello world")
 })
 
-app.get("/all", function(req, res) {
-  db.scrapedData.find({}, function(error, found) {
+app.get("/all", function (req, res) {
+  db.scrapedData.find({}, function (error, found) {
     if (error) {
       console.log(error);
     }
-    else{
+    else {
       res.json(found);
     }
   });
 });
 
 
-app.listen(4000, function() {
+app.listen(4000, function () {
   console.log("App listening on: http://localhost:4000");
 });
 
-// scraper needs to scrape: headline / summary / url 
-  // need to use axios to call the site and return a response
-  // parse response.data using cheerio
-  //  document.querySelector("#site-content > div:nth-child(3) > div:nth-child(5)")
-app.get("/scrape", function(req, res) {
-  axios.get("https://www.theonion.com/").then(function(response){
+
+app.get("/scrape", function (req, res) {
+  axios.get("https://www.theonion.com/").then(function (response) {
     const $ = cheerio.load(response.data);
-  
-    // title of article
-    // $(".sc-759qgu-0").each(function(i, element){
-    //   var title = $(element).text();
-    //   console.log(title)
-    // })
-    // $(".js_link sc-1out364-0 fwjlmD").each(function(i, element){
-    //   var link = $(element).attr("href")
-    //   console.log(link)
-    // })
-    // document.querySelector("body > div:nth-child(6) > div.sc-2d2qtd-0.dmSVnl > main > div > div.sc-17uq8ex-0.ljcHun")
+
     let results = [];
-    
-    $("article").each(function(i, element){
+
+    $("article").each(function (i, element) {
       let result = {};
-      
+
       result.title = $(this)
-      .find("a")
-      .children("h1")
-      .text();
+        .find("a")
+        .children("h1")
+        .text();
 
       result.link = $(this)
-      .find("figure")
-      .children("a")
-      .attr("href")
+        .find("figure")
+        .children("a")
+        .attr("href")
 
       console.log(result)
+      results.push(result)
+    });
+
+    db.scrapedData.insert(results, function(err, inserted){
+      if(err) {
+        console.log(err);
+      }
+      else {
+        console.log(inserted)
+      }
     })
+    
+
+
+      // .then(dbScrapedData => {
+      //   console.log("Articles added" + dbScrapedData);
+      //   res.send("scrape done");
+      // })
+      // .catch(err => {
+      //   console.log(err);
+      //   res.send(err);
+      // })
   })
+  res.send("scrape done")
 })
