aeb26a9722a7ee0df79e3763ab6e3e14668a561a
BETeng <brian7.teng@gmail.com>
2019-07-29 15:15:36 -0500

    comment nav works

--- controllers/htmlController.js
+++ controllers/htmlController.js
@@ -22,25 +22,27 @@ module.exports = function (app) {
           .attr("href")
         result.notes = [];
 
-        console.log(result)
-        results.push(result)
-
-        if (result.link) {
-          db.Article.create(results, { unordered: true })
-            .then(dbArticles => {
-              console.log('added new articles' + dbArticles);
-              res.send('scapre done')
-            })
-            .catch(err => {
-              console.log(err);
-              res.send(err);
-            })
-        }
+        // console.log(result)
+          results.push(result)
+        
+        console.log(results)
       })
+
+      db.Article.create(results, { unordered: true })
+        .then(dbArticles => {
+          console.log('added new articles' + dbArticles);
+          res.send('scapre done')
+        })
+        .catch(err => {
+          console.log(err);
+          res.send(err);
+        })
     });
   })
-  app.get("/", function(req, res) {
-    db.Article.find({}).sort({_id: -1})
+
+
+  app.get("/", function (req, res) {
+    db.Article.find({}).sort({ _id: -1 })
       .then(dbArticles => {
         res.render("index", { dbArticles });
       })
@@ -48,6 +50,47 @@ module.exports = function (app) {
         res.json(err);
       });
   });
-  
+
+  app.get("/comments/:id", function(req, res) {
+    db.Article.findOne({ _id: req.params.id })
+      .populate("notes")
+      .then(function(dbArticle) {
+        res.render("comments", { dbArticle });
+      })
+      .catch(function(err) {
+        res.json(err);
+      });
+  });
+
+  app.post("/comments/:id", function(req, res) {
+    db.Note.create(req.body)
+      .then(function(dbNote) {
+        return db.Article.findOneAndUpdate(
+          { _id: req.params.id },
+          { $push: { notes: dbNote._id } },
+          { new: true, useFindAndModify: false }
+        );
+      })
+      .then(function(dbArticle) {
+        res.end();
+      })
+      .catch(function(err) {
+        res.json(err);
+      });
+  });
+
+  app.delete("/comments/:articleid/:commentid", function(req, res) {
+    db.Note.deleteOne({ _id: req.params.commentid }).then(dbNote => {
+      db.Article.findOneAndUpdate(
+        { _id: req.params.articleid },
+        { $pull: { notes: req.params.commentid } },
+        { useFindAndModify: false }
+      ).then(dbArticle => {
+        res.end();
+      });
+    });
+  });
+
+
 }
 
--- models/Article.js
+++ models/Article.js
@@ -9,7 +9,7 @@ var ArticleSchema = new Schema({
   },
   link: {
     type: String,
-    required: true
+    required: false
   },
   notes: [{
     type: Schema.Types.ObjectId,
